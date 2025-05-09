8a210307f2ecbdc47f13ea67cba51535ffa11437
Philippe Fanaro <philippefanaro@gmail.com>
2025-05-09 14:59:29 +0900

    minor edit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit"
+
+import { env } from ".env"
+
+export default defineConfig({
+  schema: "./src/server/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: env.POSTGRES_URL,
+  },
+  tablesFilter: ["magi_*"],
+})
